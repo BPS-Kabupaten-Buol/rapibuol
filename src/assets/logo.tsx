eead06920b606858153e4caafab70425f9c95123
kudaair <damarsepti627@gmail.com>
2026-04-11 11:49:13 +0800

    fix: fix build error

--- src/assets/logo.tsx
+++ src/assets/logo.tsx
@@ -1,8 +1,6 @@
-import { type SVGProps } from 'react'
-import { cn } from '@/lib/utils'
 import { FolderKanban } from 'lucide-react'
 
-export function Logo({ className, ...props }: SVGProps<SVGSVGElement>) {
+export function Logo() {
   return (
     <div className='flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground'>
       <FolderKanban className='size-4' />
